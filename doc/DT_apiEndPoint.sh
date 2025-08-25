72bedcb878f4c8cfc69aaa79c997a98939f9a798
opensourcecontributor72 <softwarelearning2512@gmail.com>
2025-08-26 00:18:36 +0530

    update the curl command

--- doc/DT_apiEndPoint.sh
+++ doc/DT_apiEndPoint.sh
@@ -1,4 +1,4 @@
- curl -X POST https://dependency-track.tools.aa.st/api/v1/user/login      -H "Content-Type: application/x-www-form-urlencoded"      -d "username=dekuma&password=Shyam#123"
+ curl -X POST https://serverUrl/api/v1/user/login      -H "Content-Type: application/x-www-form-urlencoded"      -d "username=&password="
 
 
  curl -X POST https://your-dtrack-url/api/v1/user/login \
